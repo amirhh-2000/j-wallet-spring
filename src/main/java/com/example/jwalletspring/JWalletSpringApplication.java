14aa65dcc71aa03b33fbc9a5eec8c06576c73c7d
Amir Hossein <54184618+amirhh-2000@users.noreply.github.com>
2025-12-17 15:09:05 +0330

    init

---
+++ src/main/java/com/example/jwalletspring/JWalletSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwalletspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JWalletSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JWalletSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwalletspring/JWalletSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwalletspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JWalletSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
