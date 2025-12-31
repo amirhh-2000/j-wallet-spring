060eb5ba2cd098db8184418d013506a41e3a4042
Amir Hossein <54184618+amirhh-2000@users.noreply.github.com>
2025-12-31 17:33:16 +0330

    connection pool in RestTemplate.

--- src/main/java/com/example/jwalletspring/config/AppConfig.java
+++ src/main/java/com/example/jwalletspring/config/AppConfig.java
@@ -1,10 +1,13 @@
 package com.example.jwalletspring.config;
 
 import io.netty.resolver.DefaultAddressResolverGroup;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.netty.http.client.HttpClient;
@@ -14,7 +17,21 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        org.apache.hc.client5.http.classic.HttpClient httpClient = HttpClientBuilder.create()
+                .setConnectionManager(connectionManager)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(
+                (org.apache.hc.client5.http.classic.HttpClient) httpClient
+        );
+
+        factory.setReadTimeout(3000);
+
+        return new RestTemplate(factory);
     }
 
     @Bean
