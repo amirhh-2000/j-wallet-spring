202b37fca24d7197021b0f75bbfc20796376766a
Amir Hossein <54184618+amirhh-2000@users.noreply.github.com>
2026-01-04 13:15:12 +0330

    add threadpool.

--- src/main/java/com/example/jwalletspring/config/AppConfig.java
+++ src/main/java/com/example/jwalletspring/config/AppConfig.java
@@ -6,14 +6,34 @@ import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.netty.http.client.HttpClient;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
 @Configuration
+@EnableAsync
 public class AppConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setMaxPoolSize(10);
+        executor.setThreadNamePrefix("JWallet-Async");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        executor.initialize();
+        return executor;
+    }
 
     @Bean
     public RestTemplate restTemplate() {
